bab688ed2ff8a2023797a3386272a4bcbfba1a51
JhonatanIT <jhonatan.ibatac@gmail.com>
2023-08-01 22:22:18 +0930

    feat: add drawer with export todos data in csv

--- lib/pages/todos_page.dart
+++ lib/pages/todos_page.dart
@@ -1,8 +1,15 @@
 import 'package:example_app/database/todo_db.dart';
 import 'package:example_app/widget/e_todo_widget.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:example_app/model/todo.dart';
 import 'package:intl/intl.dart';
+
+import 'dart:io';
+import 'package:csv/csv.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class TodosPage extends StatefulWidget {
   const TodosPage({super.key});
@@ -58,6 +65,9 @@ class _TodosPageState extends State<TodosPage> {
           ),
           leading: const Icon(Icons.import_export),
           onTap: () {
+            getCsv();
+            ScaffoldMessenger.of(context)
+                .showSnackBar(SnackBar(content: Text("Todos exported!!")));
             Navigator.pop(context);
           },
         ),
@@ -164,5 +174,65 @@ class _TodosPageState extends State<TodosPage> {
         },
       ),
     );
+  }
+
+  getCsv() async {
+    List<Todo> todos = await todoDB.fetchAll();
+    List<List<dynamic>> exportData = List<List<dynamic>>.empty(growable: true);
+
+    //Headers
+    List<dynamic> headers = List.empty(growable: true);
+    headers.add("id");
+    headers.add("title");
+    headers.add("createdAt");
+    headers.add("updatedAt");
+    exportData.add(headers);
+
+    for (var todo in todos) {
+      List<dynamic> row = List.empty(growable: true);
+      row.add(todo.id);
+      row.add(todo.title);
+      row.add(todo.createdAt);
+      row.add(todo.updatedAt);
+      exportData.add(row);
+    }
+
+    if (await Permission.storage.request().isGranted) {
+      //store file in documents folder
+
+      //TODO no use external directories like Downloads
+      //TODO update to ios SO
+      //final file = await _localFile;
+      final file = File('/storage/emulated/0/Download/todos_export.csv');
+
+      // convert rows to String and write as csv file
+      String csv = const ListToCsvConverter().convert(exportData);
+      file.writeAsString(csv);
+
+      //TODO No work: alternative method to download
+      //_launchUrl();
+    } else {
+      Map<Permission, PermissionStatus> statuses = await [
+        Permission.storage,
+      ].request();
+    }
+  }
+
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/todos_export.csv');
+  }
+
+  Future<void> _launchUrl() async {
+    final Uri _url =
+        Uri.parse('file:/storage/emulated/0/Download/todos_export.csv');
+    if (!await launchUrl(_url)) {
+      throw Exception('Could not launch $_url');
+    }
   }
 }
--- lib/widget/e_todo_widget.dart
+++ lib/widget/e_todo_widget.dart
@@ -1,12 +1,5 @@
 import 'package:example_app/model/todo.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
-import 'dart:io';
-import 'package:csv/csv.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class CreateTodoWidget extends StatefulWidget {
   final Todo? todo;
@@ -39,31 +32,27 @@ class _CreateTodoWidgetState extends State<CreateTodoWidget> {
       title: Text(isEditing ? 'Edit Todo' : 'Add Todo'),
       content: Form(
         key: formKey,
-        child: Column(
-          children: [
-            TextFormField(
-              autofocus: true,
-              controller: controller,
-              decoration: const InputDecoration(hintText: 'Title'),
-              validator: (value) =>
-                  value != null && value.isEmpty ? 'Title is required' : null,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                color: Colors.green,
-                height: 50,
-                child: TextButton(
-                  child: Text(
-                    "Export to CSV",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onPressed: getCsv,
-                ),
-              ),
-            ),
-          ],
+        child: TextFormField(
+          autofocus: true,
+          controller: controller,
+          decoration: const InputDecoration(hintText: 'Title'),
+          validator: (value) =>
+              value != null && value.isEmpty ? 'Title is required' : null,
         ),
+        // Padding(
+        //   padding: const EdgeInsets.all(8.0),
+        //   child: Container(
+        //     color: Colors.green,
+        //     height: 50,
+        //     child: TextButton(
+        //       child: Text(
+        //         "Export to CSV",
+        //         style: TextStyle(color: Colors.white),
+        //       ),
+        //       onPressed: getCsv,
+        //     ),
+        //   ),
+        // ),
       ),
       actions: [
         TextButton(
@@ -80,58 +69,5 @@ class _CreateTodoWidgetState extends State<CreateTodoWidget> {
         ),
       ],
     );
-  }
-
-  getCsv() async {
-    List<List<dynamic>> employeeData =
-        List<List<dynamic>>.empty(growable: true);
-    for (int i = 0; i < 5; i++) {
-//row refer to each column of a row in csv file and rows refer to each row in a file
-      List<dynamic> row = List.empty(growable: true);
-      row.add("Employee Name $i");
-      row.add((i % 2 == 0) ? "Male" : "Female");
-      row.add(" Experience : ${i * 5}");
-      employeeData.add(row);
-    }
-
-    //TODO replace employee with full data of todos
-    //Use the export button in a DrawerHEader
-
-    if (await Permission.storage.request().isGranted) {
-      //store file in documents folder
-
-      //TODO no use external directories like Downloads
-      //final file = await _localFile;
-      final file = File('/storage/emulated/0/Download/todos_export.csv');
-
-      // convert rows to String and write as csv file
-      String csv = const ListToCsvConverter().convert(employeeData);
-      file.writeAsString(csv);
-
-      //TODO No work: alternative method to download
-      //_launchUrl();
-    } else {
-      Map<Permission, PermissionStatus> statuses = await [
-        Permission.storage,
-      ].request();
-    }
-  }
-
-  Future<String> get _localPath async {
-    final directory = await getApplicationDocumentsDirectory();
-    return directory.path;
-  }
-
-  Future<File> get _localFile async {
-    final path = await _localPath;
-    return File('$path/todos_export.csv');
-  }
-
-  Future<void> _launchUrl() async {
-    final Uri _url =
-        Uri.parse('file:/storage/emulated/0/Download/todos_export.csv');
-    if (!await launchUrl(_url)) {
-      throw Exception('Could not launch $_url');
-    }
   }
 }
